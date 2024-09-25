e3224a2c834e8df27a03716b9d628b288d7f23d4
Ryan Dostalek <rdostale@calpoly.edu>
2024-09-25 11:57:39 -0700

    Passed characterData to table via props (IE1, 4)

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Table from "./Table";
 
+const characters = [
+  {
+    name: "Charlie",
+    job: "Janitor"
+  },
+  {
+    name: "Mac",
+    job: "Bouncer"
+  },
+  {
+    name: "Dee",
+    job: "Aspiring actress"
+  },
+  {
+    name: "Dennis",
+    job: "Bartender"
+  }
+];
+
 function MyApp() {
   return (
     <div className="container">
-      <Table />
+      <Table characterData={characters} />
     </div>
   );
 }
